dd3e000420601bb82fe80b14e48f56cceb93049b
sdhaene <stephan.dhaene@cegeka.com>
2018-05-23 23:06:13 +0200

    JWT token config for REST API.

---
+++ src/main/java/com/ambradev/oauth2app/Oauth2appApplication.java
@@ -0,0 +1,12 @@
+package com.ambradev.oauth2app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2appApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ambradev/oauth2app/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.ambradev.oauth2app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import static org.springframework.core.Ordered.LOWEST_PRECEDENCE;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@Order(LOWEST_PRECEDENCE)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .formLogin().disable() // disable form authentication
+            .anonymous().disable() // disable anonymous user
+            .httpBasic().and()
+            // restricting access to authenticated users
+            .authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication() // creating user in memory
+            .withUser("user")
+            .password("password").roles("USER")
+            .and().withUser("admin")
+            .password("password").authorities("ROLE_ADMIN");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // provides the default AuthenticationManager as a Bean
+        return super.authenticationManagerBean();
+    }
+
+    //TODO: SDH Replace with proper password encoder.
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
